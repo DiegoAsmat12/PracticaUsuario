90535fcce516b428ae1fc5e7a844aeb761625f07
DiegoAsmat12 <diegoas1205@gmail.com>
2022-01-17 16:06:09 -0500

    Agregando encadenación de metodos

--- usuario.py
+++ usuario.py
@@ -5,34 +5,25 @@ class Usuario:
         self.balance_cuenta = 0
     def hacer_deposito(self,amount):
         self.balance_cuenta+=amount
+        return self
     def hacer_retiro(self,amount):
         self.balance_cuenta-=amount
+        return self
     def mostrar_balance_usuario(self):
         print("User: %s, Balance: %d"%(self.name,self.balance_cuenta))
     def transferir_dinero(self, other_user,amount):
         self.hacer_retiro(amount)
         other_user.hacer_deposito(amount)
+        return self
 
 diego = Usuario("Diego","diego@example.com")
-diego.hacer_deposito(1200)
-diego.hacer_deposito(1550)
-diego.hacer_deposito(1400)
-diego.hacer_retiro(1350)
-diego.mostrar_balance_usuario()
+diego.hacer_deposito(1200).hacer_deposito(1550).hacer_deposito(1400).hacer_retiro(1350).mostrar_balance_usuario()
 
 gabriel = Usuario("Gabriel","gabriel@example.com")
-gabriel.hacer_deposito(1350)
-gabriel.hacer_deposito(1350)
-gabriel.hacer_retiro(400)
-gabriel.hacer_retiro(800)
-gabriel.mostrar_balance_usuario()
+gabriel.hacer_deposito(1350).hacer_deposito(1350).hacer_retiro(400).hacer_retiro(800).mostrar_balance_usuario()
 
 marco = Usuario("Marco","marco@example.com")
-marco.hacer_deposito(200)
-marco.hacer_retiro(250)
-marco.hacer_retiro(250)
-marco.hacer_retiro(250)
-marco.mostrar_balance_usuario()
+marco.hacer_deposito(200).hacer_retiro(250).hacer_retiro(250).hacer_retiro(250).mostrar_balance_usuario()
 
 diego.transferir_dinero(marco,800)
 
